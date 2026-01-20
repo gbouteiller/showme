d534dc0cd630f5f230045cc3fb7fa09563d8b891
Gregory Bouteiller <gregory.bouteiller@gmail.com>
2026-01-20 13:50:01 +0400

    Update episodes.$episodeId.tsx

--- src/routes/episodes.$episodeId.tsx
+++ src/routes/episodes.$episodeId.tsx
@@ -1,9 +1,9 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/episodes/$episodeId')({
+export const Route = createFileRoute("/episodes/$episodeId")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div>Hello "/episodes/$episodeId"!</div>
+  return <div>Hello "/episodes/$episodeId"!</div>;
 }
