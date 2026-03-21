878cc6b49794f06c07c813e7ffe1d7371421100b
Gregory Bouteiller <gregory.bouteiller@gmail.com>
2026-03-21 14:02:01 +0400

    Update crons.ts

--- convex/crons.ts
+++ convex/crons.ts
@@ -3,6 +3,7 @@ import { api } from "./_generated/api";
 
 const crons = cronJobs();
 
-crons.daily("refresh all shows", { hourUTC: 5, minuteUTC: 0 }, api.shows.refreshAllDaily);
+crons.daily("refresh all shows (morning)", { hourUTC: 5, minuteUTC: 0 }, api.shows.refreshAllDaily);
+crons.daily("refresh all shows (evening)", { hourUTC: 17, minuteUTC: 0 }, api.shows.refreshAllDaily);
 
 export default crons;
