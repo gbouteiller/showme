dc66fa7c2d9ae3b2c8b6f9a9e6a8060d982a7c62
Gregory Bouteiller <gregory.bouteiller@gmail.com>
2026-03-23 11:16:16 +0400

    Update schema.ts

--- convex/schema.ts
+++ convex/schema.ts
@@ -1,22 +1,22 @@
 import { defineSchema, defineTable } from "convex/server";
 import { Schema as S } from "effect";
 import { FIELDS } from "./effex/fields";
-import { tableFrom } from "./effex/schemas/table";
+import { convexSchemaFrom } from "./effex/schemas";
 
 // SCHEMA ----------------------------------------------------------------------------------------------------------------------------------
 export default defineSchema({
-  casts: defineTable(tableFrom(S.Struct(FIELDS.casts))),
-  channels: defineTable(tableFrom(S.Struct(FIELDS.channels))).index("by_api", ["apiId"]),
-  characters: defineTable(tableFrom(S.Struct(FIELDS.characters))).index("by_api", ["apiId"]),
-  fetcher: defineTable(tableFrom(S.Struct(FIELDS.fetcher))),
-  countries: defineTable(tableFrom(S.Struct(FIELDS.countries))).index("by_code", ["code"]),
-  episodes: defineTable(tableFrom(S.Struct(FIELDS.episodes)))
+  casts: defineTable(convexSchemaFrom(S.Struct(FIELDS.casts))),
+  channels: defineTable(convexSchemaFrom(S.Struct(FIELDS.channels))).index("by_api", ["apiId"]),
+  characters: defineTable(convexSchemaFrom(S.Struct(FIELDS.characters))).index("by_api", ["apiId"]),
+  fetcher: defineTable(convexSchemaFrom(S.Struct(FIELDS.fetcher))),
+  countries: defineTable(convexSchemaFrom(S.Struct(FIELDS.countries))).index("by_code", ["code"]),
+  episodes: defineTable(convexSchemaFrom(S.Struct(FIELDS.episodes)))
     .index("by_api", ["apiId"])
     .index("by_show_and_season", ["showId", "season", "airstamp"])
     .index("by_show", ["showId", "airstamp"]),
   // .index("by_preference_and_unwatched", ["preference", "isWatched", "airstamp"]),
-  persons: defineTable(tableFrom(S.Struct(FIELDS.persons))).index("by_api", ["apiId"]),
-  shows: defineTable(tableFrom(S.Struct(FIELDS.shows)))
+  persons: defineTable(convexSchemaFrom(S.Struct(FIELDS.persons))).index("by_api", ["apiId"]),
+  shows: defineTable(convexSchemaFrom(S.Struct(FIELDS.shows)))
     .index("by_api", ["apiId"])
     // .index("by_rating", ["rating"])
     // .index("by_weight", ["weight"])
